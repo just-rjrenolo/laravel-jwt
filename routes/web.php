9862582fa4fa6ce03140ecd5315f4099b423080b
Christopher <christopherssalvador@gmail.com>
2020-07-01 14:12:55 +0800

    Laravel with JWT

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::post('register', 'UserController@register');
+// Route::post('login', 'UserController@authenticate');
+// Route::get('open', 'DataController@open');
+
+// Route::group(['middleware' => ['jwt.verify']], function() {
+//     Route::get('user', 'UserController@getAuthenticatedUser');
+//     Route::get('closed', 'DataController@closed');
+// });
